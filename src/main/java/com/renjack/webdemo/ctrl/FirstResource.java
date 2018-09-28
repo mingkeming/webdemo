36adca9fa5cecf8b5d6632c7405de22978730332
名可名 <mingkeming2009@126.com>
2018-09-28 21:11:35 +0800

    delete import

--- src/main/java/com/renjack/webdemo/ctrl/FirstResource.java
+++ src/main/java/com/renjack/webdemo/ctrl/FirstResource.java
@@ -1,19 +1,17 @@
 package com.renjack.webdemo.ctrl;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import javax.validation.Valid;
-
 import com.renjack.webdemo.service.FirstService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Michael on 2/28/2018.
