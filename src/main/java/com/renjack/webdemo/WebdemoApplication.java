642803c39ca2e9835a6770a1182899275599f3f6
名可名 <renshaobo@introtec.cn>
2020-04-21 14:59:58 +0800

    使用动态配置操作相关类，提交部分小修改

--- src/main/java/com/renjack/webdemo/WebdemoApplication.java
+++ src/main/java/com/renjack/webdemo/WebdemoApplication.java
@@ -1,12 +1,7 @@
 package com.renjack.webdemo;
 
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.SchedulerFactory;
-import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
@@ -14,22 +9,10 @@ import org.springframework.core.env.Environment;
 //@EnableSwagger2 开启swagger
 public class WebdemoApplication {
 
-	public static void main(String[] args) throws SchedulerException {
+	public static void main(String[] args){
         SpringApplication app = new SpringApplication(WebdemoApplication.class);
         Environment env = app.run(args).getEnvironment();
         System.out.println(env.getProperty("server.port"));
         System.out.println("this is ok");
-//
-//        SchedulerFactory sfact = new StdSchedulerFactory();
-//        Scheduler scheduler = sfact.getScheduler();
-//        scheduler.start();
 	}
-
-	@Bean
-    public Scheduler scheduler() throws SchedulerException {
-	    SchedulerFactory sfact = new StdSchedulerFactory();
-        Scheduler scheduler = sfact.getScheduler();
-        scheduler.start();
-        return  scheduler;
-    }
 }
--- src/main/java/com/renjack/webdemo/ctrl/FirstResource.java
+++ src/main/java/com/renjack/webdemo/ctrl/FirstResource.java
@@ -32,7 +32,7 @@ public class FirstResource {
     private Scheduler scheduler;
 
     @PostMapping(value = "/insertBatch")
-    public Map changeJobSwitch() throws SchedulerException {
+    public Map changeJobSwitch(){
         //参数传递数组
         //String q = redisService.get("applet_access_token1");
         Map<String,Object> retData = new HashMap<>();
@@ -52,20 +52,20 @@ public class FirstResource {
         return retData;
     }
 
-    @PostMapping(value = "/changeJob")
-    public Map changeJob() throws SchedulerException {
-        changeTheJob("0/5 * * * * ?");
-        return new HashMap<>();
-    }
-
     private void changeTheJob(String corn) throws SchedulerException {
-        JobDetail jobDetail = JobBuilder.newJob(HelloJob.class).withIdentity("myJob1","group1").build();
-        Trigger trigger = TriggerBuilder.newTrigger().withIdentity("myTrigger1","group1").startNow()
-                .withSchedule(CronScheduleBuilder.cronSchedule(corn)).build();
+        try{
+            //JobDetail jobDetail = JobBuilder.newJob((Class<? extends Job>) Class.forName("com.renjack.webdemo.tools.HelloJob")).withIdentity("myJob1","group1").build();
+            JobDetail jobDetail = JobBuilder.newJob((Class<? extends Job>) Class.forName("com.renjack.webdemo.tools.HelloJob")).withIdentity("myJob1","group1").build();
+            Trigger trigger = TriggerBuilder.newTrigger().withIdentity("myTrigger1","group1").startNow()
+                    .withSchedule(CronScheduleBuilder.cronSchedule(corn)).build();
 
-        //需要将jobDetail和trigger传进去，并将jobDetail和trigger绑定在一起。
-        JobKey jobKey = new JobKey("myJob1","group1");
-        scheduler.deleteJob(jobKey);
-        scheduler.scheduleJob(jobDetail,trigger);
+            //需要将jobDetail和trigger传进去，并将jobDetail和trigger绑定在一起。
+            JobKey jobKey = new JobKey("myJob1","group1");
+            scheduler.deleteJob(jobKey);
+            scheduler.scheduleJob(jobDetail,trigger);
+        }catch(Exception e){
+            System.out.println(1);
+        }
+
     }
 }
